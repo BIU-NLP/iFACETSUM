a2dcb34f1969fc4dd1ad4cfd08dbbb84b1bb2630
Ori Shapira <obspp18@gmail.com>
2020-09-17 18:02:06 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0650/referenceSummaries/D0650.M.250.E.C
@@ -0,0 +1,11 @@
+In 1982, former US president Jimmy Carter and his wife founded the Carter Center, devoted to observing countries and furthering their democracy and development, human rights issues, and public health.
+Carter Center observer missions, often including former heads of state, have monitored at least 20 elections in 16 countries, including China, the Dominican Republic, Guyana, Indonesia, Liberia, Mexico, Mozambique, Nicaragua, Nigeria, Panama, Peru, and Venezuela.
+Carter's involvement helped persuade Ortega to accept Nicaraguan election results that deposed him, persuade Cedras to leave Haiti and avoid a US invasion, win freedom for thousands of political prisoners, and possibly avert war in Korea.
+The Carter Center has received funding from Coca-Cola for a project to encourage openness in government, combat corruption, and curtail bribery in Latin America.
+Thugs have threatened Carter's life.
+He has been criticized for getting in the way and for announcing deals to CNN before telling Washington.
+Since 1987, the Carter Center has worked to treat and prevent diseases causing blindness in Africa, Latin America, and the Middle East, including trachoma and river blindness.
+It is responsible for the near global eradication of Guinea worm disease.
+It has been aided by a Lions Club grant and medicines donated by Merck and Pfizer.
+Carter has become known as the greatest ex-president in US history.
+He has written books on elections and the Middle East, visited 115 countries on humanitarian missions, been awarded the Presidential Medal of Freedom, and earned adulation throughout Latin America and Africa.
