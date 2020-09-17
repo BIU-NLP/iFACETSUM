679b20b973416f5e0f38837985c5544c22fe827a
Ori Shapira <obspp18@gmail.com>
2020-09-17 17:38:57 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0630/referenceSummaries/D0630.M.250.C.C
@@ -0,0 +1,12 @@
+Bookselling is changing dramatically.
+Big chains are proliferating and squeezing out independent bookstores.
+Independents lost half their market share in six years and many have closed.
+Most books are now sold in nontraditional outlets, including discount stores and the internet.
+The American Bookseller Association represents private bookstore owners and sponsors Book Expo, an annual convention.
+The ABA brought antitrust suits against top publishing houses and Borders and Barnes & Noble for making secret deals that undercut independents, resulting in a $25 million payment by publishers to booksellers.
+It censured author Frank McCourt for participating in chainstore advertising, challenged barnesandnoble.com's claim "If we don't have your book nobody does," led successful opposition to B&N's plan to purchase the largest book distributor (threatening independents with reduced service and compromised records), and joined the Northern California Independent Booksellers Association in urging collection of taxes from on-line bookstores.
+To meet online competition, the ABA opened Book Sense and Contentville, online selling and commentary sites serving as hubs for individual stores.
+Online book sales captured 2% of the market in 1999 and were projected to rise to 10% by 2002 and 18% by 2003.
+Online competition led barnesandnoble.com to sell shares to the public, B&N and Amazon to establish their own distribution centers to increase profits, and barnesandnoble.com and borders.com to match amazon.com in halving best-seller prices.
+Online sales hurt mail-order and book club sales.
+To stay competitive, traditional booksellers increased personal service, sponsored special events, and began selling online and offering broad search capabilities.
