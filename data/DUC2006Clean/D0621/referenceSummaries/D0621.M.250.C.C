40f2cd0caa566e75fd44e12002987dbd0e65fbb5
Ori Shapira <obspp18@gmail.com>
2020-09-17 17:30:16 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0621/referenceSummaries/D0621.M.250.C.C
@@ -0,0 +1,17 @@
+Criminal activity in China is rising, especially murder, robbery, bombings, theft, gang activity, juvenile delinquency, and migrant felonies.
+Prostitution, gambling, and drug problems persist.
+Corruption and bribery is still widespread.
+Sixty percent of criminals arrested are minors.
+In April 1996 Chinese police launched a nationwide crackdown on serious crime with emphasis on gang-related crimes, those involving guns, and gun manufacture and smuggling.
+Many gangs were smashed, arrests made, and many criminals were sentenced to death or life in prison.
+Bank robber and killer Liao Yanxiong was executed.
+Property, cash, guns, ammunition, gun-making tools, explosives, and vehicles were seized.
+Police were often aided by tips from citizens.
+China passed new laws to strengthen human rights and amended criminal laws to enhance the fight against organized crime, money-laundering, terrorism, trafficking in illegal immigrants, and environment-related crime.
+China plans to crack down on corruption, smuggling, and other economic crimes.
+China wants to strengthen international cooperation in fighting crime.
+It signed judicial assistance agreements with 28 countries and extradition agreements with ten.
+Chinese and Hong Kong police jointly crushed a cross-border gang led by Cheung Tza-keung (Big Spender), which kidnapped tycoons' family members for $205 million ransom, among other crimes.
+Cheung incriminated his henchman Chan Chi-ho.
+China crushed a mainland gang related to a larger Macao gang and pledged increased anti-crime cooperation with Macao.
+It urged Portugal to get tougher on Macao crime before the colony's return to China in 1999, after which China would station troops there and deal more harshly with criminals.
