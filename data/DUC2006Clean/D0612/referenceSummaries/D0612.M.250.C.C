dbb07c38a4bcbd8fa64039e69e9c7f72f2009d35
Ori Shapira <obspp18@gmail.com>
2020-09-17 17:19:33 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0612/referenceSummaries/D0612.M.250.C.C
@@ -0,0 +1,23 @@
+Diagnosis of autism is based on observation of communication, behavior and development.
+Signs appear during the first three years, including oddly uncoordinated movements of babies.
+Autistic children have brain differences and tend to have bigger heads.
+A third suffer gastrointestinal problems.
+Their blood has abnormally high levels of four proteins.
+The only widely accepted treatment is applied behavioral analysis, involving repetitive teaching that breaks learning into incremental tasks with positive reinforcement.
+Early intervention has best results.
+Only half of children respond well.
+Repeated treatments with the digestive hormone secretin and antibiotic vancomycin have had positive results.
+Drugs sometimes control aggressive and self-destructive behavior.
+Research has involved MRIs, establishment of gene and tissue banks, and breeding of Guinea pigs with autistic abnormalities.
+Also studied are videos of autistic babies and bowel problem connections.
+The University of Washington leads in autism research.
+Autism is caused by interference in brain development from interactions between genes on Chromosomes 7, 13, and 15 and environmental factors, either during pregnancy or after birth.
+The broad spectrum of symptoms suggests multiple causes.
+Sex hormone influence explains the preponderance of boys.
+Suggested triggers include milk or wheat allergies, viruses, environmental toxins, maternal drugs, and peptides created by digestive problems.
+Support services are expensive and insufficient.
+New Jersey, Massachusetts, and Pennsylvania have superior programs.
+Autism affects 1 in 500-1000 and is on the rise.
+Eighty percent are boys.
+Odds are 1 in 20 that a family with one autistic child will have another.
+Brick, NJ has an autism cluster.
