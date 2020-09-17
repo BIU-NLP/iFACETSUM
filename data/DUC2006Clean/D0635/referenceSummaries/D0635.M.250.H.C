5e5ab35950a5bc823a57be97520152660a3b50dd
Ori Shapira <obspp18@gmail.com>
2020-09-17 17:44:48 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0635/referenceSummaries/D0635.M.250.H.C
@@ -0,0 +1,15 @@
+Texas Governor George W. Bush defended the death penalty as a deterrent to crime and presided over a record 143 executions.
+In five years he halted only one, because of faulty evidence, and granted one 30-day reprieve.
+He based decisions on doubt about guilt and court opportunity for reviewing legal issues.
+He believed no innocent people were executed and that he had no right to overturn jury verdicts.
+He could recommend clemency only if the 18-member Board of Pardons and Paroles, all of whom he appointed, recommended it.
+Board members held no meetings and voted by fax or telephone.
+Bush opposed public clemency meetings as opportunities for emotionalizing the process.
+Bush speeded the appeals process, opposed barring execution of the mentally retarded, vetoed creation of impartial public defender systems, and said he favored DNA testing but did not push to require it.
+Texas' clemency process has been criticized widely as violating minimum safeguards for human rights.
+The parole board has been described as hostile, secretive, unconstitutional, unfair to indigent defendants, and interested only in preserving the system and the illusion of review.
+Proceedings are undocumented so there is no way to determine whether voting is arbitrary or meets due process requirements.
+Few letters are forwarded to board members.
+The Pope and others unsuccessfully asked Bush to stay executions for a Canadian citizen, a born-again woman, and the mentally ill.
+Bush has been criticized for callousness and being disengaged.
+The public still supports the death penalty but support is shrinking dramatically.
---
+++ data/DUC2006Clean/D0637/referenceSummaries/D0637.M.250.A.C
@@ -0,0 +1,18 @@
+Solar energy is non-polluting, renewable, and low-cost.
+Its use preserves the environment and helps develop the economy.
+Solar energy is the most affordable way to bring electricity to remote or hard-to-wire areas that are "off the grid", such as deserts, jungles and mountains.
+Solar boats could open up waterways that have been banned to human use because of associated pollution.
+Sunny places like North China have great solar energy potential.
+Solar energy is popular in developing countries worldwide, including Cameroun, Brazil, Egypt, India, Indonesia, Mexico, Somalia, Uganda, and Vietnam.
+It is used in heating, cooling, lighting, cooking, refrigeration, telecommunications, meteorology, earthquake forecasts, and forest fire prevention.
+Countries that have made advancements in solar energy, such as Israel, Germany, Japan, and Canada, are assisting with solar energy development in China and Africa.
+China is also aiding African solar energy development.
+In the next 10 years, 253 cooperative solar demonstration projects are to be launched worldwide.
+Australia is a leader in solar energy research, with Australian universities developing efficient ultra-thin solar cells and high-tech material for solar thermal electricity.
+The US leads in solar equipment manufacturing.
+It also leads in solar energy production, followed by Japan and Europe.
+Germany is expanding solar cell production.
+China is a major solar energy producer and sells it at home and abroad.
+The Netherlands is increasing its solar energy use and installing collectors on rooftops.
+Israel is a leading solar energy user and does extensive research and development.
+Brazil and Mexico are major solar power consumers.
