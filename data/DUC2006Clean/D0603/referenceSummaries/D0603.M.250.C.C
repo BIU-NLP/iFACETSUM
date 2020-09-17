fb1a720844777809277b1c95278fb7baf7cde654
Ori Shapira <obspp18@gmail.com>
2020-09-17 17:09:43 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0603/referenceSummaries/D0603.M.250.C.C
@@ -0,0 +1,18 @@
+Wetlands, called earth's kidney, are important in filtering pollutants, regulating climate, conserving water, preventing erosion, controlling floods, and preserving biodiversity.
+They can be low-cost extensions of wastewater treatment plants.
+Wetlands are of vital economic importance to Asia.
+Human activities threaten wetlands in many places, including Asia, Africa, and the US, where 100,000 acres per year have been lost.
+The 1971 Ramsar Convention, aiming to conserve wetland habitats and enhance international cooperation, has 90 signatory countries.
+In 1996 Wetlands International linked government and NGO groups to work for conservation.
+Countries are raising public awareness of wetlands' importance.
+In 1996 Uganda established an environmental abuse reporting hot line and banned new draining of swamps for development.
+China established over 130 wetland nature reserves since 1982 and increased wetland protections.
+International help with wetland restoration has come from the Netherlands, Finland, and the World Bank.
+In the US, the 1972 Clean Water Act mandated Corps of Engineers regulation of wetlands.
+The ineffective nationwide permit system was tightened in 1996 to require individual permits for projects of 3-acres instead of 10, then in 2000 half an acre, and one-tenth an acre in the NY City watershed.
+Closer scrutiny was required.
+Developers who damaged wetlands were required to create new ones.
+Declarations of critical habitat for wildlife also limited development.
+Illegal projects have been shut down and fined.
+Protecting wetlands has been hindered by insufficient enforcement; pressure from politicians, developers and industry; and resistance of residents.
+Side effects have included mosquito breeding and beaver damage.
