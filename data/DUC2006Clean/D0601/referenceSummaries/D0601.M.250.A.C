9f5d1f22966e1194d37cec1d6630ed884eb96bb2
Ori Shapira <obspp18@gmail.com>
2020-09-17 17:04:47 +0300

    Add files via upload

---
+++ data/DUC2006Clean/D0601/referenceSummaries/D0601.M.250.A.C
@@ -0,0 +1,18 @@
+Half of Native American Indians on reservations live in poverty with poor health care, substandard housing, and limited computer access.
+Drug and alcohol addiction, domestic violence, poor education, teen births, and lack of jobs creates unemployment 10 times the national average.
+Violent crime is increasing, especially on poor reservations.
+Continuing prejudice against Indians results in drunken attacks by whites.
+Police are insufficient and ill-equipped.
+Some tribes generate significant income from businesses and manufacturing.
+One-third of tribes offer gambling.
+Casinos aren't always successful and are often opposed by localities that don't want local gambling or competitors who fear untaxed competition.
+Many Indians leave the reservation.
+Others worry that tribal values and customs are deteriorating.
+Reservations grant tribes self-rule and reserve sovereign land for them.
+The federal government pays for reservation clinics, schools, and welfare.
+Indians on reservations with 50% unemployment are exempt from welfare caps.
+State taxes are not levied on Indians' reservation income.
+Tribes can tax non-Indian reservation employees.
+They can determine local zoning, fishing and hunting rules; ban alcohol; and open casinos.
+Tribal authorities deal with minor crimes but turn to federal help for more serious crime.
+Problems facing Indians and the reservation system include decreased funding for Indian programs; repeal of affirmative action laws; Indian culture and local regulations conflicting with federal or state laws, tribes not being fully recognized and having no access to federal funds; and banks unwilling to give Indians home loans that are backed by land held in trust.
