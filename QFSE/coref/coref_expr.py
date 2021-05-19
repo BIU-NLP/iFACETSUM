9fb8157a14070b29b9e8dde1a7e1df2e7d155cf3
Alon Eirew <alon.eirew@intel.com>
2021-05-19 11:25:49 +0300

    remove code to handle mention span and coref cluster id (redundant)

--- QFSE/coref/coref_expr.py
+++ QFSE/coref/coref_expr.py
@@ -12,15 +12,15 @@ def get_clusters(data) -> Tuple[Dict[str, List[Mention]], Dict[int, List[Mention
         cluster_id = int(ment['coref_chain'])
         tok_star = int(ment['tokens_number'][0])
         tok_end = int(ment['tokens_number'][-1])
-        if tok_end - tok_star < 5:
-            if doc_id not in document:
-                document[doc_id] = list()
-            if cluster_id not in clusters:
-                clusters[cluster_id] = list()
-            ment_obj = Mention(doc_id, int(ment['sent_id']), tok_star, tok_end,
-                               ment['tokens_str'], cluster_id, COREF_TYPE_EVENTS)
+        ment_obj = Mention(doc_id, int(ment['sent_id']), tok_star, tok_end,
+                           ment['tokens_str'], cluster_id, COREF_TYPE_EVENTS)
 
-            document[doc_id].append(ment_obj)
-            clusters[cluster_id].append(ment_obj)
+        if doc_id not in document:
+            document[doc_id] = list()
+        if cluster_id not in clusters:
+            clusters[cluster_id] = list()
+
+        document[doc_id].append(ment_obj)
+        clusters[cluster_id].append(ment_obj)
 
     return document, clusters
