743f3bdf883a2915fa92e73d543247f05c199c71
Eran Hirsch <hirscheran@gmail.com>
2021-05-21 17:58:13 +0300

    - Remove singletons (#16)
    - Improve usage flow
    - Fix bug with embedded token groups

--- QFSE/coref/coref_expr.py
+++ QFSE/coref/coref_expr.py
@@ -6,7 +6,6 @@ from QFSE.coref.models import Mention
 
 def get_clusters(data) -> Tuple[Dict[str, List[Mention]], Dict[int, List[Mention]]]:
     clusters = dict()
-    document = dict()
     for ment in data:
         doc_id = ment['doc_id'][ment['doc_id'].index("_")+1:]
         cluster_id = int(ment['coref_chain'])
@@ -15,12 +14,22 @@ def get_clusters(data) -> Tuple[Dict[str, List[Mention]], Dict[int, List[Mention
         ment_obj = Mention(doc_id, int(ment['sent_id']), tok_star, tok_end,
                            ment['tokens_str'], cluster_id, COREF_TYPE_EVENTS)
 
-        if doc_id not in document:
-            document[doc_id] = list()
         if cluster_id not in clusters:
             clusters[cluster_id] = list()
 
-        document[doc_id].append(ment_obj)
         clusters[cluster_id].append(ment_obj)
 
-    return document, clusters
+    singletons = [c_id for c_id, c in clusters.items() if len(c) < 2]
+    for singleton_cluster_id in singletons:
+        clusters.pop(singleton_cluster_id)
+
+    # Create documents only after filtering clusters
+    documents = dict()
+    for mentions in clusters.values():
+        for mention in mentions:
+            if mention.doc_id not in documents:
+                documents[mention.doc_id] = list()
+
+            documents[mention.doc_id].append(mention)
+
+    return documents, clusters
